6e1584002e9cf6651bbdc63b9536d15968ea75cd
Henry Robert Muwanika <hrmuwanika@gmail.com>
2026-01-28 17:40:07 +0200

    Refactor Docker GPG key and repository setup
    Updated Docker GPG key handling and repository setup in the Kubernetes worker setup script.

--- scripts/setup_k8s_worker.sh
+++ scripts/setup_k8s_worker.sh
@@ -78,14 +78,15 @@ EOF
 
 sudo sysctl --system
 
+# Add the Docker GPG Key and Repository
+sudo mkdir -p /etc/apt/keyrings
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
  # Add the repository to Apt sources:
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update the system:
 sudo apt update
@@ -95,7 +96,7 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 
 # Start and enable Docker service
 sudo systemctl start docker
-sudo systemctl enable docker
+sudo systemctl enable --now docker
 
 # Install kubeadm
 VERSION=$(curl -s https://api.github.com/repos/kubernetes/kubernetes/releases/latest | jq -r '.tag_name')
